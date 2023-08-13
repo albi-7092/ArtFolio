cfc5560627c9c6f6ce2b53c4191e6f52624a7a20
Albin Antony <albinantony7092@gmail.com>
2023-08-13 17:17:51 +0530

    Final

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@
 import 'package:artfolio/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,24 +18,7 @@ class app extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ArtFolio',
-      home: MyApp(),
-    );
-  }
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
       home: Splash(),
-      theme: ThemeData(
-        primaryColor: Color(0xFF17203A),
-      ),
     );
   }
 }
